efcb65fb1d5b7734b80b0b80ea322a89aea7e2ce
jhxxr <2471717907@qq.com>
2021-09-25 19:00:42 +0800

    Site updated: 2021-09-25 19:00:42

--- yzmigu/static_mi/js/list.js
+++
@@ -1,8 +0,0 @@
-function pageLoad(){
-
-	// 初始化导航
-	$('#conditionBox .s-slide-menu').mySlideMenu();
-
-	pageLoaded = true;
-}
-if(typeof(pageLoaded) == 'undefined' && typeof(jsApi) != 'undefined'){ pageLoad(); }
